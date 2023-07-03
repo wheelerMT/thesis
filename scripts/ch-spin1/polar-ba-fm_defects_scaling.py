65acaa991ad3c970b68986244f643b7829c757fd
Matt Wheeler <wheelermt@proton.me>
2023-07-03 17:57:38 +0100

    Add text

---
+++ scripts/ch-spin1/polar-ba-fm_defects_scaling.py
@@ -0,0 +1,36 @@
+import h5py
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+
+plt.rcParams["text.usetex"] = True
+plt.rc("text.latex", preamble=r"\usepackage{fourier}")
+plt.rcParams.update({"font.size": 16})
+matplotlib.use("TkAgg")
+
+quenches = [i for i in range(200, 1000, 100)] + [
+    i for i in range(1000, 8500, 500)
+]
+
+num_of_domains = []
+domains_std = []
+for quench in quenches:
+    with h5py.File("../thesis_data/1d_polar-BA-FM_domains.hdf5", "r") as file:
+        nd_total = file[f"{quench}/nd_total"][...]
+        domains_std.append(np.std(nd_total))
+        num_of_domains.append(sum(nd_total) / len(nd_total))
+
+fig, ax = plt.subplots(1)
+ax.set_ylabel(r"$N_d$")
+ax.set_xlabel(r"$\tau_Q$")
+ax.errorbar(
+    quenches,
+    num_of_domains,
+    yerr=domains_std,
+    capsize=5,
+    ecolor="k",
+    fmt="none",
+)
+ax.loglog(quenches, num_of_domains, "ko")
+ax.loglog(quenches[:10], 8.5e2 * np.array(quenches[:10]) ** (-1 / 4), "k--")
+plt.savefig("gfx/ch-spin1/polar-BA-FM_domains.pdf", bbox_inches="tight")
