509c41c918156ec59048c4a9f33c259cae5e17b5
Matt Wheeler <wheelermt@proton.me>
2023-07-04 13:14:34 +0100

    Add text and new plots

--- scripts/ch-spin1/ba-fm_defects_scaling.py
+++ scripts/ch-spin1/ba-fm_defects_scaling.py
@@ -15,12 +15,12 @@ quenches = [i for i in range(200, 1000, 100)] + [
 num_of_domains = []
 domains_std = []
 for quench in quenches:
-    with h5py.File("../thesis_data/1d_polar-BA-FM_domains.hdf5", "r") as file:
+    with h5py.File("../thesis_data/1d_BA-FM_domains.hdf5", "r") as file:
         nd_total = file[f"{quench}/nd_total"][...]
         domains_std.append(np.std(nd_total))
         num_of_domains.append(sum(nd_total) / len(nd_total))
 
-fig, ax = plt.subplots(1)
+fig, ax = plt.subplots(1, figsize=(6.4, 3.8))
 ax.set_ylabel(r"$N_d$")
 ax.set_xlabel(r"$\tau_Q$")
 ax.errorbar(
@@ -33,10 +33,11 @@ ax.errorbar(
 )
 ax.loglog(quenches, num_of_domains, "ko")
 ax.loglog(
-    quenches[:10],
-    8.5e2 * np.array(quenches[:10]) ** (-1 / 4),
+    quenches[:11],
+    11.5e2 * np.array(quenches[:11]) ** (-1 / 4),
     "k--",
     label=r"$\tau_Q^{-1/4}$",
 )
 plt.legend()
-plt.savefig("gfx/ch-spin1/polar-BA-FM_domains.pdf", bbox_inches="tight")
+plt.savefig("gfx/ch-spin1/FM_domains_scaling.pdf", bbox_inches="tight")
+plt.show()
