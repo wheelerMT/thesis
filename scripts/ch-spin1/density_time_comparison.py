e92d387fe5b6cfaaf112e3e0ffd54be71dd2989d
Matt Wheeler <wheelermt@proton.me>
2023-07-05 16:14:43 +0100

    Add text

--- scripts/ch-spin1/density_time_comparison.py
+++ scripts/ch-spin1/density_time_comparison.py
@@ -8,35 +8,35 @@ plt.rc("text.latex", preamble=r"\usepackage{fourier}")
 plt.rcParams.update({"font.size": 26})
 matplotlib.use("TkAgg")
 
-data_5000 = h5py.File("../thesis_data/1d_BA-FM_500.hdf5", "r")
+data_500 = h5py.File("../thesis_data/1d_BA-FM_500.hdf5", "r")
 data_1000 = h5py.File("../thesis_data/1d_BA-FM_1000.hdf5", "r")
 
 index_range = slice(8196 - 1024, 8196 + 1024)
 x = data_1000["grid/x"][index_range]
-time_5000 = data_5000["time/t"][:, 0]
+time_500 = data_500["time/t"][:, 0]
 time_1000 = data_1000["time/t"][:, 0]
-psi_1_5000 = data_5000["wavefunction/psi_plus"][index_range, :]
+psi_1_500 = data_500["wavefunction/psi_plus"][index_range, :]
 psi_1_1000 = data_1000["wavefunction/psi_plus"][index_range, :]
 
 num_of_frames = psi_1_1000.shape[-1]
 spacetime_plus_1000 = np.empty((2048, num_of_frames))
-spacetime_plus_5000 = np.empty((2048, num_of_frames))
+spacetime_plus_500 = np.empty((2048, num_of_frames))
 
 for i in range(num_of_frames):
     spacetime_plus_1000[:, i] = abs(psi_1_1000[:, i]) ** 2
-    spacetime_plus_5000[:, i] = abs(psi_1_5000[:, i]) ** 2
+    spacetime_plus_500[:, i] = abs(psi_1_500[:, i]) ** 2
 
-extent = 0, time_5000.max(), x.min(), x.max()
+extent = 0, time_500.max(), x.min(), x.max()
 
 fig, ax = plt.subplots(2)
-ax[0].set_xlim(0, time_5000.max())
+ax[0].set_xlim(0, time_500.max())
 ax[0].set_xticks([])
-ax[1].set_xlim(0, time_5000.max())
+ax[1].set_xlim(0, time_500.max())
 for axis in ax:
     axis.set_ylabel(r"$x/\xi_s$", labelpad=-25)
 ax[1].set_xlabel(r"$t/\tau$")
 plot = ax[0].pcolormesh(
-    time_5000, x[::2], spacetime_plus_5000[::2, :], vmin=0, vmax=1
+    time_500, x[::2], spacetime_plus_500[::2, :], vmin=0, vmax=1
 )
 ax[1].pcolormesh(time_1000, x[::2], spacetime_plus_1000[::2, :], vmin=0, vmax=1)
 
